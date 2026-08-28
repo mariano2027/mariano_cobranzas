58a366a00b6cd06e226de2a7b20aeed84624eab5
mariano2027 <marianopaez153@gmail.com>
2026-08-28 10:53:33 -0300

    Add files via upload

--- mariano_cobranzas.py
+++ mariano_cobranzas.py
@@ -845,6 +845,6 @@ def export_dinamica(n_clicks, records, vendedor_sel, cliente_sel):
     
 
     if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False)
+        port = int(os.environ.get('PORT', 8080))
+        app.run(host='0.0.0.0', port=port, debug=False)
 
